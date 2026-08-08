0db7c7bde32f1493cd71d5009f317c872fb04f4a
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-07 22:57:47 -0400

    Expand GTA runtime pointers

--- src/Integrations/GTA5_Enhanced/Memory/GTA_Pointers.hpp
+++ src/Integrations/GTA5_Enhanced/Memory/GTA_Pointers.hpp
@@ -1,12 +1,38 @@
 #pragma once
+
 #include "Backend/Memory/Pointer.hpp"
 
-namespace Devilz::Integrations::GTA5_Enhanced {
-struct GTA_Pointers {
-    Backend::Pointer nativeTable{};
-    Backend::Pointer gameState{};
-    Backend::Pointer scriptGlobals{};
-    Backend::Pointer frameCount{};
-    [[nodiscard]] bool Ready() const noexcept { return nativeTable && gameState; }
+namespace Devilz::Integrations::GTA5_Enhanced
+{
+struct GTA_Pointers
+{
+    Devilz::Backend::Pointer nativeTable{};
+    Devilz::Backend::Pointer initNativeTables{};
+    Devilz::Backend::Pointer gameState{};
+    Devilz::Backend::Pointer scriptGlobals{};
+    Devilz::Backend::Pointer scriptThreads{};
+    Devilz::Backend::Pointer programTable{};
+    Devilz::Backend::Pointer scriptVM{};
+    Devilz::Backend::Pointer frameCount{};
+
+    [[nodiscard]] bool CoreReady() const noexcept
+    {
+        return !gameState.IsNull() && !scriptGlobals.IsNull();
+    }
+
+    [[nodiscard]] bool NativeBootstrapReady() const noexcept
+    {
+        return !initNativeTables.IsNull();
+    }
+
+    [[nodiscard]] bool ScriptReady() const noexcept
+    {
+        return !scriptGlobals.IsNull() && !scriptThreads.IsNull() && !programTable.IsNull();
+    }
+
+    [[nodiscard]] bool Ready() const noexcept
+    {
+        return CoreReady() && ScriptReady();
+    }
 };
 }
