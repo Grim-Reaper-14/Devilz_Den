9dfefdc7930a8595ed384942634f0dbb6ebf1ea4
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-09 18:40:28 -0400

    Add DLL runtime manager interface

---
+++ src/Runtime/Runtime_Manager.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Backend/Logging/LoggerService.hpp"
+#include "Backend/Threading/ThreadManager.hpp"
+#include "Integrations/GTA5_Enhanced/GTA_Module_Manager.hpp"
+
+#include <atomic>
+
+namespace Devilz
+{
+class Runtime_Manager final
+{
+public:
+    Runtime_Manager() = default;
+    ~Runtime_Manager();
+
+    Runtime_Manager(const Runtime_Manager&) = delete;
+    Runtime_Manager& operator=(const Runtime_Manager&) = delete;
+
+    [[nodiscard]] bool Start();
+    void Stop() noexcept;
+
+    [[nodiscard]] bool Running() const noexcept { return m_running.load(); }
+
+private:
+    void LogGTAStatus(const Integrations::GTA5_Enhanced::GTA_Module_Status& status);
+
+    Backend::LoggerService m_logger;
+    Backend::ThreadManager m_threads;
+    Integrations::GTA5_Enhanced::GTA_Module_Manager m_gta;
+    std::atomic_bool m_running{false};
+};
+}
