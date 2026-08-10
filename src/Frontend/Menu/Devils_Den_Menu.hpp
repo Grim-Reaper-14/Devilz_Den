2b7473b67005992ef07c3c7b2f05ced1ce6c2682
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-10 16:24:25 -0400

    Add Devils Forge vehicle menu state

--- src/Frontend/Menu/Devils_Den_Menu.hpp
+++ src/Frontend/Menu/Devils_Den_Menu.hpp
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <cstddef>
+#include <cstdint>
 
 namespace Devilz::Frontend
 {
@@ -24,6 +25,7 @@ private:
     void DrawBanner();
     void DrawNavigation();
     void DrawSelfPage();
+    void DrawVehiclePage();
     void DrawTeleportPage();
     void DrawPlaceholderPage(const char* title, const char* detail);
 
@@ -37,5 +39,23 @@ private:
     bool m_infiniteAmmo = false;
     bool m_fastRun = false;
     float m_health = 100.0F;
+
+    std::array<char, 64> m_vehicleSearch{};
+    int m_vehicleClassFilter = -1;
+    std::uint32_t m_selectedVehicleModel = 0;
+    bool m_spawnInsideVehicle = true;
+    bool m_spawnVehicleMaxed = false;
+    bool m_spawnVehicleOnGround = true;
+    bool m_spawnVehicleEngineRunning = true;
+    bool m_spawnVehicleInvincible = false;
+    bool m_spawnVehicleClean = true;
+    int m_forgeModSlot = 11;
+    int m_forgeModIndex = -1;
+    int m_forgeWheelType = 7;
+    int m_forgePrimaryPaintType = 1;
+    int m_forgePrimaryColor = 0;
+    int m_forgePearlescent = 0;
+    int m_forgeSecondaryPaintType = 1;
+    int m_forgeSecondaryColor = 0;
 };
 }
