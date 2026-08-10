90d67523883bd22ff2b4f4a57722e72f10aa4f36
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-10 14:02:07 -0400

    Declare Dear ImGui Win32 message handler

--- src/Frontend/Renderer/D3D12_Renderer.cpp
+++ src/Frontend/Renderer/D3D12_Renderer.cpp
@@ -11,6 +11,12 @@
 #include <string>
 #include <utility>
 
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(
+    HWND hWnd,
+    UINT msg,
+    WPARAM wParam,
+    LPARAM lParam);
+
 namespace Devilz::Frontend
 {
 namespace
